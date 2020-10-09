a0ecf0e5077a459c4c29d72cdfa67f205f317366
Mujeeb <mujeebmjrhaman@gmial.com>
2020-10-09 22:48:51 +0530

    edit button disable for drinks and breads

--- lib/widget/cart_item.dart
+++ lib/widget/cart_item.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:test_demo22/model/ProductModel.dart';
 import 'package:test_demo22/model/cart.dart';
+import 'package:test_demo22/viewmodel/ProductCatVM.dart';
 import 'package:test_demo22/viewmodel/ProductVM.dart';
 import 'package:test_demo22/widget/round_button_indian_bread.dart';
 import '../pages/product_details.dart';
@@ -44,135 +46,151 @@ class _CartItem1State extends State<CartItem1> {
   @override
   void initState() {
     // TODO: implement initState
-    try {
-      Provider.of<SubmenuListVM>(context, listen: false)
-          .fetchProduct(int.parse(widget.catID))
-          .then((value) => {});
-    } on Exception catch (e) {
+    try {} on Exception catch (e) {
       // TODO
     }
   }
 
+  Future<void> callforUpdate() async {
+    await Provider.of<SubmenuListVM>(context, listen: false)
+        .fetchProduct(int.parse(widget.catID));
+
+    var cat = Provider.of<SubmenuListVM>(context, listen: false)
+        .productList
+        .where((element) => element.catid == int.parse(widget.catID));
+
+    cat.forEach((element) {
+      if (element.product.id == int.parse(widget.id)) {
+        print(element.product.name);
+        menu = element.product;
+        print(element.product.id);
+      }
+    });
+    // return menu;
+  }
+
+  bool isBreadOrDrink() {
+    var isTrue = false;
+    var bread = Provider.of<ProductListVM>(context, listen: false).breadsVM;
+    if (bread.first.id.toString() == widget.catID) {
+      return isTrue = true;
+    }
+    var drink = Provider.of<ProductListVM>(context, listen: false).drinksVM;
+    if (drink.first.id.toString() == widget.catID) {
+      return isTrue = true;
+    }
+    return isTrue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    var menu;
-
     final cart = Provider.of<Cart>(context);
-    //menu = Provider.of<SubmenuListVM>(context,listen:false).productList;
-    // Provider.of<SubmenuListVM>(context, listen: false).fetchProduct().then((value) => {
-
-    // });
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 5, vertical: 4),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: Image.memory(
-            base64Decode(widget.imgLoc),
-          ),
-          title: Row(
-            children: <Widget>[
-              //================================= Title of the item in the cart ======================
-              Text(widget.title),
-              //================================= - Button ==============================
-              RoundIconButtonIndianBread(
-                color: Colors.red,
-                icon: Icons.remove,
-                onPressed: () {
-                  cart.subtractItemCount(
-                      widget.productId,
-                      widget.catID,
-                      widget.price,
-                      widget.title,
-                      widget.quantity,
-                      widget.imgLoc,
-                      widget.spicyLevel,
-                      widget.typedDescription,
-                      widget.description);
-                },
+    return FutureBuilder<void>(
+        future: callforUpdate(),
+        builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Card(
+              margin: EdgeInsets.symmetric(horizontal: 5, vertical: 4),
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: ListTile(
+                  leading: Image.memory(
+                    base64Decode(widget.imgLoc),
+                  ),
+                  title: Row(
+                    children: <Widget>[
+                      //================================= Title of the item in the cart ======================
+                      Text(widget.title),
+                      //================================= - Button ==============================
+                      RoundIconButtonIndianBread(
+                        color: Colors.red,
+                        icon: Icons.remove,
+                        onPressed: () {
+                          cart.subtractItemCount(
+                              widget.productId,
+                              widget.catID,
+                              widget.price,
+                              widget.title,
+                              widget.quantity,
+                              widget.imgLoc,
+                              widget.spicyLevel,
+                              widget.typedDescription,
+                              widget.description);
+                        },
+                      ),
+                      //=================================== Quantity ============================
+                      Text("${widget.quantity}"),
+                      //================================== + Button =============================
+                      RoundIconButtonIndianBread(
+                        color: Colors.green,
+                        icon: Icons.add,
+                        onPressed: () {
+                          cart.addItemCount(
+                              widget.productId,
+                              widget.catID,
+                              widget.price,
+                              widget.title,
+                              widget.quantity,
+                              widget.imgLoc,
+                              widget.spicyLevel,
+                              widget.typedDescription,
+                              widget.description);
+                        },
+                      ),
+                    ],
+                  ),
+                  trailing: Text("\$${widget.price}"),
+                  subtitle: Row(
+                    children: <Widget>[
+                      //=================================== Edit Button =============================
+                      FlatButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            side: BorderSide(color: Colors.grey)),
+                        onPressed: isBreadOrDrink()
+                            ? null
+                            : () {
+                          print(isBreadOrDrink());
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => ProductDetails(
+                                              userVM: widget.userVm,
+                                              menuitem: menu,
+                                              quantity: widget.quantity,
+                                              flag: 1,
+                                              passedSpicyLevel:
+                                                  widget.spicyLevel,
+                                              passedDescription:
+                                                  widget.typedDescription,
+                                            )));
+                              },
+                        child: Text('Edit'),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      //====================================Remove Button ==============================
+                      FlatButton(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            side: BorderSide(color: Colors.grey)),
+                        onPressed: () {
+                          Provider.of<Cart>(context, listen: false)
+                              .removeItem(widget.productId);
+                        },
+                        child: Text('Remove'),
+                      )
+                    ],
+                  ),
+                ),
               ),
-              //=================================== Quantity ============================
-              Text("${widget.quantity}"),
-              //================================== + Button =============================
-              RoundIconButtonIndianBread(
-                color: Colors.green,
-                icon: Icons.add,
-                onPressed: () {
-                  cart.addItemCount(
-                      widget.productId,
-                      widget.catID,
-                      widget.price,
-                      widget.title,
-                      widget.quantity,
-                      widget.imgLoc,
-                      widget.spicyLevel,
-                      widget.typedDescription,
-                      widget.description);
-                },
-              ),
-            ],
-          ),
-          trailing: Text("\$${widget.price}"),
-          subtitle: Row(
-            children: <Widget>[
-              //=================================== Edit Button =============================
-              FlatButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    side: BorderSide(color: Colors.grey)),
-                onPressed: () {
-                  var cat = Provider.of<SubmenuListVM>(context, listen: false)
-                      .productList
-                      .where((element) =>
-                          element.catid == int.parse(widget.catID));
-
-                  cat.forEach((element) {
-                    if (element.product.id == int.parse(widget.id)) {
-                      print(element.product.name);
-                      menu = element.product;
-                    }
-                  });
-//
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => ProductDetails(
-                                userVM: widget.userVm,
-                                menuitem: menu,
-                                quantity: widget.quantity,
-                                flag: 1,
-                                passedSpicyLevel: widget.spicyLevel,
-                                passedDescription: widget.typedDescription,
-                              )));
-//                    'itemCardId': widget.id,
-//                    'itemCardTitle': widget.title,
-//                    'itemCardImgLoc': widget.imgLoc,
-//                    'itemCardDescription': widget.description,
-//                    'itemCardPrice': widget.price,
-//                    'itemCardQuantity': widget.quantity,
-//                    'itemCardSpicyLevel': widget.spicyLevel,
-//                    'itemCardTypedDescription': widget.typedDescription
-                },
-                child: Text('Edit'),
-              ),
-              SizedBox(
-                width: 5,
-              ),
-              //====================================Remove Button ==============================
-              FlatButton(
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                    side: BorderSide(color: Colors.grey)),
-                onPressed: () {
-                  Provider.of<Cart>(context, listen: false)
-                      .removeItem(widget.productId);
-                },
-                child: Text('Remove'),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
+            );
+          }
+          return Center(child: CircularProgressIndicator());
+        });
   }
 }
